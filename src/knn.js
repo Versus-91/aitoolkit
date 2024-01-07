db02bf73ed48c2325688ce6576703c3f8af97edc
Ghader <gharibyan.ghader@gmail.com>
2024-01-07 22:33:00 +0100

    changing knn

--- src/knn.js
+++ src/knn.js
@@ -1,22 +1,53 @@
-import KNN from 'ml-knn';
+import KNN from "ml-knn"
 
 import { KNeighborsClassifier } from 'scikitjs'
 export default class KNNModel {
+
     constructor() {
         this.model = null
     }
-    async train(x_train, y_train, k = 3) {
-        this.model = new KNeighborsClassifier({ nNeighbors: k })
-        await this.model.fit(x_train, y_train);
+    async train(x_train, y_train, k = 3, distance = manhatan_distance) {
+        this.model = new KNN(x_train, y_train, { k: k, distance: distance });
     }
     predict(x_test) {
         if (this.model === null || this.model === undefined) {
             throw "model not found."
         }
-        var ans = window.tf.tidy(() => {
-            let results = this.model.predict(x_test);
-            return Array.from(results.dataSync())
-        })
-        return ans
+        return this.model.predict(x_test)
     }
+}
+function squaredEuclidean(p, q) {
+    let d = 0;
+    for (let i = 0; i < p.length; i++) {
+        if (typeof p[i] === "string" && typeof p[i] === "string") {
+            if (p[i] === q[i]) {
+                d += 0
+            } else {
+                d += 1
+            }
+        } else {
+            d += (p[i] - q[i]) * (p[i] - q[i]);
+        }
+        console.log(d)
+    }
+    return d;
+}
+function manhatan_distance(p, q) {
+    let d = 0;
+    for (let i = 0; i < p.length; i++) {
+        if (typeof p[i] === "string" && typeof p[i] === "string") {
+            if (p[i] === q[i]) {
+                d += 0
+            } else {
+                d += 1
+            }
+        } else {
+            d += Math.abs(p[i] - q[i])
+        }
+        console.log(d);
+    }
+    return d;
+}
+function euclidean(p, q) {
+    return Math.sqrt(squaredEuclidean(p, q));
 }
