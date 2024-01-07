f26c947c51a9811444f281848145bc79533731b1
Ghader <gharibyan.ghader@gmail.com>
2024-01-07 22:33:11 +0100

    sm freeing memory

--- src/svm.js
+++ src/svm.js
@@ -8,6 +8,8 @@ export default class SupportVectorMachine {
     }
     predict(x_test) {
         const result = this.model.predict(x_test);
+        console.log(this.model.serializeModel());
+        this.model.free();
         return result
     }
 }
